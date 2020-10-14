f11484af863a931a69d16a4fae6d685562d51936
Ben Chislett <ben.chislett@getmysa.com>
2020-10-14 19:42:48 -0230

    Add bit layout comment to move structure

--- engine/src/move.h
+++ engine/src/move.h
@@ -5,6 +5,10 @@
 
 struct Move
 {
+  // First 6 bits are source square (first 3 are file, next 3 are rank)
+  // Next 6 bits are destination square (same layout)
+  // Next 2 bits are unused move type bits, last 2 are promotion bits:
+  // 00: promote to knight, 01: promote to bishop, 10: promote to rook, 11: promote to queen
   uint16_t data;
 
   Move() {}
